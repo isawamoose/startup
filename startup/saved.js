3772067558ee1bafa96712f40f157ff78b0d5ac2
Stephen Amos <amos.stephen.m@gmail.com>
2023-03-10 17:32:37 -0700

    fixed song save / delete

--- startup/saved.js
+++ startup/saved.js
@@ -1,35 +1,30 @@
 const songs = JSON.parse(localStorage.getItem('songs')) ?? [];
-const itemElArray = [];
+const itemEls = {};
 const savedSongListEl = document.querySelector('#displayList');
-count = 0;
-// const songMap = new Map();
 
 function displaySongs() {
-	songs.forEach((song) => {
+	for (const song of Object.values(songs)) {
 		const savedItem = document.createElement('div');
 		savedItem.classList.add('saved-item');
 
 		const savedSong = document.createElement('li');
 		savedSong.classList.add('saved-song');
 		savedSong.innerText = song.title;
-		savedSong.id = 'song-' + count;
+		savedSong.id = 'song-' + song.id;
 		savedSong.addEventListener('click', openSong);
 
 		const deleteButton = document.createElement('button');
 		deleteButton.classList.add('delete', 'text-light');
 		deleteButton.innerText = 'X';
-		deleteButton.id = 'delete-' + count;
+		deleteButton.id = 'delete-' + song.id;
 		deleteButton.addEventListener('click', deleteSong);
 
 		savedItem.appendChild(savedSong);
 		savedItem.appendChild(deleteButton);
 
-		// songMap.set(count, song);
-		// console.log(songMap);
-		count++;
-		itemElArray.push(savedItem);
+		itemEls[song.id] = savedItem;
 		savedSongListEl.appendChild(savedItem);
-	});
+	}
 }
 
 function openSong(event) {
@@ -41,11 +36,12 @@ function openSong(event) {
 function deleteSong(event) {
 	// Array indexing adjusts when you remove an element
 	console.log('click');
-	const idNum = event.target.id.replace(/^\D+/g, '');
-	// delete songs[idNum];
+	const id = event.target.id.replace(/^\D+/g, '');
+	delete songs[id];
+	console.log(songs);
 	localStorage.setItem('songs', JSON.stringify(songs));
 
-	itemElArray[idNum].remove();
+	itemEls[id].remove();
 }
 
 displaySongs();
